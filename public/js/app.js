7a00c912ff41e4be571014695140ac965af38656
Samuel Weisse <samuelweisse@Generals-MacBook-Air-5.local>
2016-08-01 21:43:50 -0700

    updated code with nav

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('PortfolioApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode({enabled:true});
+
+  $routeProvider.when('/', {
+		templateUrl: 'partials/home.html'
+	})
+
+}]);
