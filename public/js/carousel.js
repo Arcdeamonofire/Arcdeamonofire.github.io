d60adb81b7507eab6b57f8d40dd96eccd7adc687
Samuel Weisse <samuelweisse@Generals-MacBook-Air-5.local>
2016-08-02 13:44:02 -0700

    fix carousel

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
   $("#projects").owlCarousel({
 
-    navigation : true, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    singleItem:true
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
   });
+
 });
